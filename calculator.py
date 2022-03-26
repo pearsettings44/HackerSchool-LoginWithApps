41ee60990b616b68ab0fe39e112aa6f12fbb94ff
pearsettings44 <jose.a.pereira03@gmail.com>
2022-03-26 21:40:11 +0000

    Add docalculation() method to Calculator class.

--- calculator.py
+++ calculator.py
@@ -1,24 +1,42 @@
 import main as c
+import os
+
 
 class Calculator():
     """
     Calculator Class: Represents a calculator.
     """
+
     def __init__(self):
         self.mainmenu()
+
+    def clearscreen(self):
+        """
+        Clear the terminal's screen, using the cls command on Windows, or
+        the clear command on UNIX systems.
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        os.system('cls' if os.name == 'nt' else 'clear')
+
     def displaylogo(self):
         """
         Displays the calculator logo.
         Parameters
         ----------
         None
-        
+
         Returns
         -------
         None
         """
         logo = \
-        f"""{c.Color.BOLD}{c.Color.GREEN}
+            f"""{c.Color.BOLD}{c.Color.GREEN}
  _____   ___   _     _____ _   _ _       ___ _____ ___________ 
 /  __ \ / _ \ | |   /  __ \ | | | |     / _ \_   _|  _  | ___ \\
 | /  \// /_\ \| |   | /  \/ | | | |    / /_\ \| | | | | | |_/ /
@@ -27,14 +45,14 @@ class Calculator():
  \____/\_| |_/\_____/\____/\___/\_____/\_| |_/\_/  \___/\_| \_|
         {c.Color.END}"""
         print(logo)
-    
+
     def mainmenu(self):
         """
         Displays the main menu
         Parameters
         ----------
         None
-        
+
         Returns
         -------
         None
@@ -42,7 +60,7 @@ class Calculator():
         self.displaylogo
         print("1 )Solve basic calculations.")
         self.getoption()
-        
+
     def getoption(self):
         """
         Gets the user option.
@@ -71,10 +89,36 @@ class Calculator():
         """
         # Basic calculation option
         if option == '1':
+            self.clearscreen()
             self.docalculation()
         else:
             print("Please type a valid option! ")
             self.getoption()
-    
-    def docalculation():
+
+    def docalculation(self):
+        """
+        Uses eval to get the result of the expression given by the user.
+        Parameters
+        ----------
+        None
         
+        Results
+        -------
+        None
+        """
+        self.displaylogo()
+        print(f"\t{c.Color.BOLD}                   Please use:{c.Color.END}")
+        print(f"\t  ({c.Color.BOLD}{c.Color.GREEN}+{c.Color.END})"
+              f"for adition              ({c.Color.BOLD}{c.Color.GREEN}-"
+              f"{c.Color.END}) for subtraction:")
+        print(f"\t  ({c.Color.BOLD}{c.Color.GREEN}*{c.Color.END})"
+              f"for multiplication       ({c.Color.BOLD}{c.Color.GREEN}/"
+              f"{c.Color.END}) for division")
+        print(f"\t  ({c.Color.BOLD}{c.Color.GREEN}**{c.Color.END})"
+              f"for exponent            ({c.Color.BOLD}{c.Color.GREEN}%"
+              f"{c.Color.END}) for rest of")
+
+        expression = input("\n\n\t  Enter your expression here -> ")
+        result = eval(expression)
+        print(f"\n\t                   {c.Color.GREEN}result: {c.Color.END}"
+              f"{result}")

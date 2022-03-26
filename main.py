6af185e0b4dd92c9e09759b376eee1f4818135e5
pearsettings44 <jose.a.pereira03@gmail.com>
2022-03-26 15:26:44 +0000

    Add Menu class.

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ import os
 # Clear the terminal's screen, using the cls command on Windows, or
 # the clear command on UNIX systems.
 os.system('cls' if os.name == 'nt' else 'clear')
+
+class Menu():
+    """
+    Menu Class: handles the HackerSchool's App screen
+    """
+
+    def __init__(self):
+        pass
+

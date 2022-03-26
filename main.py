e1a7ef7d1ee0dace879c6a4acfcb6a6c31ad321b
pearsettings44 <jose.a.pereira03@gmail.com>
2022-03-26 15:27:24 +0000

    Add mainmenu method to Menu class.

--- main.py
+++ main.py
@@ -13,3 +13,25 @@ class Menu():
     def __init__(self):
         pass
 
+    def mainmenu(self):
+        """
+        Displays the main menu.
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        print("_   _            _               ____       _                 _ ")
+        print("| | | | __ _  ___| | _____ _ __  / ___|  ___| |__   ___   ___ | |")
+        print("| |_| |/ _` |/ __| |/ / _ \ '__| \___ \ / __| '_ \ / _ \ / _ \| |")
+        print("|  _  | (_| | (__|   <  __/ |     ___) | (__| | | | (_) | (_) | |")
+        print("|_| |_|\__,_|\___|_|\_\___|_|    |____/ \___|_| |_|\___/ \___/|_|")
+        print("\n\n")
+        print("    Login                  Register                  Exit ")
+        print("   type(L)                  type(R)                 type(E)")
+        print("\n")
+
+
